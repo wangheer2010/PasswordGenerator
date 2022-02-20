6595a29035f53da358e37197f9e717fd3642840a
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-02-19 21:00:34 -0500

    Added getUpper and getLower functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,60 @@
 // Assignment code here
+// Define a function to get the length of the password
+function getLength() {
+  let lengthOfPassword = prompt("Please enter the length of your password (it should be 8 to 128 characters)");
+  let realLength = 0 // Set a base value for a variable
+  if (lengthOfPassword != null && lengthOfPassword>=8 && lengthOfPassword<=128) {
+    realLength= lengthOfPassword;
+  } else {
+    realLength = getLength();
+  }
+  return realLength;
+}
 
+function getUpper() {
+  let hasUpperCase = prompt("Do you want Uppercase letter in your password? Please enter yes or no (in lower case)");
+  let hasRealUpperCase = false; // Set a base value for a variable
+  if (hasUpperCase === "yes") {
+    hasRealUpperCase= true;
+  } else if (hasUpperCase === "no") {
+    hasRealUpperCase= false;
+  } else {
+    hasRealUpperCase = getUpper();
+  }
+  return hasRealUpperCase;
+}
+
+function getLower() {
+  let hasLowerCase = prompt("Do you want Lowercase letter in your password? Please enter yes or no (in lower case)");
+  let hasRealLowerCase = false; // Set a base value for a variable
+  if (hasLowerCase === "yes") {
+    hasLowerLowerCase= true;
+  } else if (hasLowerCase === "no") {
+    hasRealLowerCase= false;
+  } else {
+    hasRealLowerCase = getLower();
+  }
+  return hasRealLowerCase;
+}
+
+
+function generatePassword() {
+  var password = ''
+  var passwordLength = getLength();
+  var UpperOrNot = getUpper(); 
+  var LowerOrNot = getLower();
+  console.log(UpperOrNot)
+  // create a set of all types of of characters (eg 1-26 upper, 27-52 lower, ...)
+  // for each index range select a number in it. for loop
+  // n conditions (n, yes or nos)
+  // random function to select n-1 random number, the nth is decided by length - (number1+number2+..number(n-1))
+  // 
+  
+
+  // random ordering function
+  // sorting
+  return password
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Password Generator Starter Code
+
+## Purpose
+
+We create a new, secure password every time we click on the button
+
+## Steps to take
+
+### Step 1
+
+ Create the prompts for password criteria using prompt function. Users can select which criteria to include in the password
+
+### Step 2
+
+Create the prompt for the length of the password. Users can choose a length of at least 8 characters and no more than 128 characters. If it is not in the range, users will be asked to do that again
+
+### Step 3
+
+Create prompts for character types to include in the password, including lowercase, uppercase, numeric, and/or special characters. We create the prompts separately asking for yes or no
+
+### Step 4
+
+Assign the answer to each prompt to a boolean and then create function so that at least one character type should be selected.
+
+### Step 5
+
+Create a function so that all prompts are answered a password is generated that matches the selected criteria
+
+### Step 6
+
+ password is generated
+THEN the password is either displayed in an alert or written to the page
